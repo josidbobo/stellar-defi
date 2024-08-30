f0e0827f0ddc3d1947308e3452b040ee50623b01
josidbobo <josidbobo@gmail.com>
2024-08-30 09:26:55 +0100

    chore: Setup

---
+++ src/components/liquidity.js
@@ -0,0 +1,6 @@
+import * as Stellar from '@stellar/wallet-sdk';
+import {account, KeyPair, TransactionBuilder, Asset, Operation, LiquidityPoolAsset, getLiquidityPoolId, BASE_FEE, Networks} from 'stellar-sdk';
+
+async function createLiquidity()  {
+    return 
+}
