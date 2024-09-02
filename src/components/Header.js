7080f96737bd1135be04cfcd675fdc8673a38a9b
josidbobo <josidbobo@gmail.com>
2024-09-02 06:22:36 +0100

    chore: Header all done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import logo from '../Assets/stellar-defi.png';
+import sLogo from '../Assets/stellar-defi2.png';
 
 function Header(){
     return( 
         <header>
-            <div className='leftH'></div>
+            <div className='leftH'>
+                <img src={logo} alt='Logo' className='logo'/>
+                <div className='headerItem'>Swap</div>
+                <div className='headerItem'>Tokens</div>
+            </div>
+            <div className='rightH'>
+                <div className='headerItem'>
+                    <img src={sLogo} alt='Secondlogo' className='eth'/>
+                    Stellar
+                </div>
+                <div className='connectButton'>
+                    Create Stellar Account
+                </div>
+            </div>
         </header>
     )
 }
